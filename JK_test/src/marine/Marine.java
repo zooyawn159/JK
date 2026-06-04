f0352f7b8644d31cec3a6e839c62a50318a5d75d
zooyawn159 <samstagsgeschenk@gmail.com>
2026-06-04 12:10:45 +0900

    Merge branch 'master' of https://github.com/zooyawn159/JK.git

--- JK_test/src/marine/Marine.java
+++ JK_test/src/marine/Marine.java
@@ -6,8 +6,7 @@ public class Marine {
             new SeaCreature("🐋 고래",  200, "우우우~"),
             new SeaCreature("🦑 오징어", 400, "....(무언의 먹물)"),
             new SeaCreature("🐙 문어",  600, "슉슉"),
-            new SeaCreature("🦈 상어",  800, "..."),
-            new SeaCreature("이제 끝!", 1000, "the end")
+            new SeaCreature("🦈 상어",  800, "...")
         };
 
         System.out.println("=== 바다 탐험 시작 ===");
